df361e6caa7d87e945f334f51d8c8e71a37a6a58
Elmir <elmir.rasulov.2002@mail.ru>
2024-05-18 00:00:06 +0300

    add dev04

--- develop/dev04/task.go
+++ develop/dev04/task.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"sort"
+	"strings"
+)
+
 /*
 === Поиск анаграмм по словарю ===
 
@@ -19,6 +24,69 @@ package main
 Программа должна проходить все тесты. Код должен проходить проверки go vet и golint.
 */
 
-func main() {
+type ByRune []rune
 
+// реализуем методы для sort.Slice
+func (b ByRune) Len() int {
+	return len(b)
+}
+
+func (b ByRune) Swap(i, j int) {
+	b[i], b[j] = b[j], b[i]
+}
+
+func (b ByRune) Less(i, j int) bool {
+	return b[i] < b[j]
+}
+
+func Anagram(m *[]string) map[string][]string {
+
+	toLower(*m)
+	*m = remDuplicate(*m)
+	// создаем карту для результата
+	res := make(map[string][]string)
+
+	for _, words := range *m {
+		// приводим строку к рунам и сортируем его
+		var tmp = ByRune(words)
+		sort.Sort(tmp)
+		// проверка на то что отсортированное слово уже есть в мапе
+		_, ok := res[string(tmp)]
+
+		if !ok {
+			// если нет то ключом становиться отсортированное слово и его изначальное состояние добавляем в значение
+			res[string(tmp)] = []string{words}
+
+		} else {
+			// по остортиванной строке добавялем изначальное состояние строки
+			res[string(tmp)] = append(res[string(tmp)], words)
+
+		}
+	}
+
+	return res
+}
+
+// приводим к нижнему регистру
+func toLower(m []string) {
+	for indx := range m {
+		m[indx] = strings.ToLower(m[indx])
+	}
+}
+
+func remDuplicate(m []string) []string {
+	duplMap := make(map[string]struct{})
+
+	for indx, i := range m {
+		_, ok := duplMap[i]
+
+		if !ok {
+			duplMap[i] = struct{}{}
+		} else {
+			m = append(m[:indx], m[indx+1:]...)
+		}
+
+	}
+
+	return m
 }
--- develop/dev04/task_test.go
+++ develop/dev04/task_test.go
@@ -1,1 +1,30 @@
 package main
+
+import (
+	// "fmt"
+	"reflect"
+	"testing"
+)
+
+func TestFindAnagram(t *testing.T) {
+	first := []string{"Пятак", "Листок", "Пятка", "Слиток", "Тяпка", "Столик", "Столик"}
+
+	expectedKeys := []string{"акптя", "иклост"}                                               // Ключи, которые ожидаются в карте
+	expectedValues := [][]string{{"пятак", "пятка", "тяпка"}, {"листок", "слиток", "столик"}} // Ожидаемые значения для каждого ключа
+
+	res := Anagram(&first)
+
+	// Проверяем, что количество элементов в результирующей карте соответствует ожидаемому
+	if len(res) != len(expectedKeys) {
+		t.Errorf("Unexpected number of elements in result map")
+	}
+
+	// Проверяем, что каждый ключ и соответствующие ему значения в карте соответствуют ожидаемым
+	for i, key := range expectedKeys {
+		if _, exists := res[key]; !exists {
+			t.Errorf("Key %q not found in result map", key)
+		} else if !reflect.DeepEqual(res[key], expectedValues[i]) {
+			t.Errorf("Value for key %q does not match expected value", key)
+		}
+	}
+}
